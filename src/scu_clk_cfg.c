a94789d4b2eac26fa8cd8d8cdfca9c4eab72dec4
dmedser <dmedser.mephi@gmail.com>
2018-10-10 13:25:08 +0300

    prj restruct

--- src/scu_clk_cfg.c
+++ src/scu_clk_cfg.c
@@ -17,7 +17,7 @@ void scu_clk_init(void) {
 	        sizeof(IfxScuCcu_testPllConfigSteps) / sizeof(IfxScuCcu_PllStepsConfig),
 	        (IfxScuCcu_PllStepsConfig *)IfxScuCcu_testPllConfigSteps,
 	        IFXSCU_CFG_PLL_INITIAL_STEP,
-		},
+	    },
 	    IFXSCU_CFG_CLK_DISTRIBUTION,
 	    IFXSCU_CFG_FLASH_WAITSTATE
 	};
