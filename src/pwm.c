80220717f09ff83a15d03555560ef8976b355cb3
dmedser <dmedser.mephi@gmail.com>
2018-10-10 13:20:40 +0300

    prj restruct

--- src/pwm.c
+++ src/pwm.c
@@ -19,8 +19,8 @@ void pwm_init(void) {
 		/* Enable FXCLK */
 		GTM_CMU_CLK_EN.B.EN_FXCLK = 0b10;
 
-		GTM_TOM0_TGC1_FUPD_CTRL.B.FUPD_CTRL4   = 0b10;
-		GTM_TOM0_TGC1_GLB_CTRL.B.UPEN_CTRL4    = 0b10;
+		GTM_TOM0_TGC1_FUPD_CTRL.B.FUPD_CTRL4 = 0b10;
+		GTM_TOM0_TGC1_GLB_CTRL.B.UPEN_CTRL4  = 0b10;
 		GTM_TOM0_CH12_SR0.B.SR0 = PWM_PERIOD;
 		GTM_TOM0_CH12_SR1.B.SR1 = PWM_PERIOD / 2;
 		GTM_TOM0_CH12_CN0.B.CN0 = 0;
@@ -32,7 +32,7 @@ void pwm_init(void) {
 		/* Service request priority number (0 - lowest, 0xFF - highest priority) */
 		MODULE_SRC.GTM.GTM[0].TOM[0][6].B.SRPN = ISR_PN_GTM_TOM0_CH12;
 		/* Enable service request */
-		MODULE_SRC.GTM.GTM[0].TOM[0][6].B.SRE  = 0b1;
+		MODULE_SRC.GTM.GTM[0].TOM[0][6].B.SRE = 0b1;
 		_install_int_handler(ISR_PN_GTM_TOM0_CH12, (void (*) (int))ISR_GTM_TOM0_CH12_cmp_match, 0);
 	}
 
