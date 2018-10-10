477910a4b967dbc96604c4bb2e44e589c6d47e5d
dmedser <dmedser.mephi@gmail.com>
2018-10-10 13:16:56 +0300

    prj restruct

--- src/edge_bit_capture.c
+++ src/edge_bit_capture.c
@@ -21,11 +21,11 @@ void edge_bit_capture_init(void) {
 		GTM_TIM0_CH0_CTRL.B.TIM_EN   	= 0b1;	/* Enable TIM0_0 */
 		GTM_TIM0_CH0_CTRL.B.TIM_MODE 	= 0b11; /* TIPM */
 		GTM_TIM0_CH0_CTRL.B.ISL     	= 0b1;  /* Ignore DSL and treat both edges as active edge */
-		GTM_TIM0_CH0_CTRL.B.FLT_EN		= 0b1;	/* Enable FLT */
+		GTM_TIM0_CH0_CTRL.B.FLT_EN	= 0b1;	/* Enable FLT */
 		GTM_TIM0_CH0_CTRL.B.FLT_CNT_FRQ = 0b00;	/* FLT_CNT counts with CMU_CLK0 */
 		GTM_TIM0_CH0_CTRL.B.FLT_MODE_RE = 0b0;	/* Immediate edge propagation mode (acceptance time) */
 		GTM_TIM0_CH0_CTRL.B.FLT_MODE_FE = 0b0;	/* Immediate edge propagation mode (acceptance time) */
-		GTM_TIM0_CH0_CNTS.B.CNTS 		= 0;	/* After 1 edge an TIM0_NEWVAL[x]_IRQ is raised */
+		GTM_TIM0_CH0_CNTS.B.CNTS 	= 0;	/* After 1 edge an TIM0_NEWVAL[x]_IRQ is raised */
 		GTM_TIM0_CH0_FLT_RE.B.FLT_RE 	= FLT_ACCEPTANCE_TIME;
 		GTM_TIM0_CH0_FLT_FE.B.FLT_FE 	= FLT_ACCEPTANCE_TIME;
 
@@ -45,10 +45,10 @@ void edge_bit_capture_init(void) {
 
 		GTM_TOM0_TGC0_FUPD_CTRL.B.FUPD_CTRL0 = 0b10;
 		GTM_TOM0_TGC0_GLB_CTRL.B.UPEN_CTRL0  = 0b10;
-		GTM_TOM0_CH0_SR1.B.SR1 				 = SECOND_HALF_OF_BIT_TX_PERIOD;
-		GTM_TOM0_CH0_SR0.B.SR0 				 = OVER_BIT_TX_PERIOD;
-		GTM_TOM0_CH0_CN0.B.CN0 				 = 0;
-		GTM_TOM0_CH0_CTRL.B.CLK_SRC_SR		 = 0;
+		GTM_TOM0_CH0_SR1.B.SR1               = SECOND_HALF_OF_BIT_TX_PERIOD;
+		GTM_TOM0_CH0_SR0.B.SR0               = OVER_BIT_TX_PERIOD;
+		GTM_TOM0_CH0_CN0.B.CN0               = 0;
+		GTM_TOM0_CH0_CTRL.B.CLK_SRC_SR       = 0;
 
 		GTM_TOM0_CH0_IRQ_EN.B.CCU1TC_IRQ_EN = 0b1;
 		GTM_TOM0_CH0_IRQ_EN.B.CCU0TC_IRQ_EN = 0b1;
