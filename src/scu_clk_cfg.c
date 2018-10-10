a0476ad563917a453fbaf25fa20a7f66e01487b3
dmedser <dmedser.mephi@gmail.com>
2018-10-10 13:24:32 +0300

    prj restruct

--- src/scu_clk_cfg.c
+++ src/scu_clk_cfg.c
@@ -5,14 +5,13 @@
 
 /* fPLL = 200 MHz */
 void scu_clk_init(void) {
-	/** \brief Default configuration for the PLL initial steps
-	 */
+	
+	/* Default configuration for the PLL initial steps */
 	static const IfxScuCcu_PllStepsConfig IfxScuCcu_testPllConfigSteps[] = {
 	    IFXSCU_CFG_PLL_STEPS
 	};
 
-	/** \brief Default configuration for the Clock Configuration
-	 */
+	/* Default configuration for the Clock Configuration */
 	IfxScuCcu_Config IfxScuCcu_testClockConfig = {
 	    {
 	        sizeof(IfxScuCcu_testPllConfigSteps) / sizeof(IfxScuCcu_PllStepsConfig),
@@ -22,6 +21,7 @@ void scu_clk_init(void) {
 	    IFXSCU_CFG_CLK_DISTRIBUTION,
 	    IFXSCU_CFG_FLASH_WAITSTATE
 	};
-    /* standard PLL initialisation */
-	IfxScuCcu_init(&IfxScuCcu_testClockConfig);
+	
+       /* Standard PLL initialisation */
+       IfxScuCcu_init(&IfxScuCcu_testClockConfig);
 }
