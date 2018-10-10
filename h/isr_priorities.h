601a88692c17fd8f20f25430463a0c42ceeacc32
dmedser <dmedser.mephi@gmail.com>
2018-10-10 13:09:48 +0300

    prj restruct

--- h/isr_priorities.h
+++ h/isr_priorities.h
@@ -7,11 +7,11 @@
  * (0 - lowest, 0xFF - highest priority)
  */
 
-#define ISR_PN_GTM_TOM0_CH12		128
-#define ISR_PN_GTM_TIM0_CH0			128
-#define ISR_PN_GTM_TOM0_CH0			129
-#define ISR_PN_GPT12_6				250
-#define ISR_PN_SW_0					249
+#define ISR_PN_GTM_TOM0_CH12    128
+#define ISR_PN_GTM_TIM0_CH0     128
+#define ISR_PN_GTM_TOM0_CH0     129
+#define ISR_PN_GPT12_6          250
+#define ISR_PN_SW_0             249
 
 
 #endif /* ISR_PRIORITIES_H_ */
