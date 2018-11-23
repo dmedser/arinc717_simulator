86309c239f4fc4251e92319df6a8585db82d9052
AG\serebryanskiy <dmedser.mephi@gmail.com>
2018-11-23 12:21:04 +0300

    v 1.0

--- src/common/can.c
+++ src/common/can.c
@@ -125,7 +125,7 @@ void can_init(void) {
 
 static uint32_t swap_endianness(uint32_t value) {
 	return ((value & ((uint32_t)0xFF << 0))  << 24) |
-           ((value & ((uint32_t)0xFF << 8))  << 8)  |
+	       ((value & ((uint32_t)0xFF << 8))  << 8)  |
            ((value & ((uint32_t)0xFF << 16)) >> 8)  |
            ((value & ((uint32_t)0xFF << 24)) >> 24);
 }
@@ -272,9 +272,9 @@ void ISR_can_tx(void) {
 		 * уникального ID каждого исходящего CAN сообщения */
 
 		uint64_t can_tx_msg_id_splitted = (uint64_t)(can_tx_msg_id & (0xF << 0))  << (12 - 0) |
-                                          (uint64_t)(can_tx_msg_id & (0xF << 4))  << (28 - 4) |
-                                          (uint64_t)(can_tx_msg_id & (0xF << 8))  << (44 - 8) |
-                                          (uint64_t)(can_tx_msg_id & (0xF << 12)) << (60 - 12);
+		                                  (uint64_t)(can_tx_msg_id & (0xF << 4))  << (28 - 4) |
+	                                      (uint64_t)(can_tx_msg_id & (0xF << 8))  << (44 - 8) |
+	                                      (uint64_t)(can_tx_msg_id & (0xF << 12)) << (60 - 12);
 
 		can_tx_msg_data |= can_tx_msg_id_splitted;
 
